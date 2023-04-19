87c78c5e5eb9994e91f0c157b347bbcbd6a4588f
jay-kalariya <agolavishwa@gmail.com>
2023-04-19 14:02:00 +0530

    updated

---
+++ script.js
@@ -0,0 +1,42 @@
+
+// For LOGIN
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+var a = document.getElementById("log");
+var b = document.getElementById("reg");
+var w = document.getElementById("other");
+
+function register() {
+  x.style.left = "-400px";
+  y.style.left = "50px";
+  z.style.left = "110px";
+  w.style.visibility = "hidden";
+  b.style.color = "white";
+  a.style.color = "black";
+}
+
+function login() {
+  x.style.left = "50px";
+  y.style.left = "450px";
+  z.style.left = "0px";
+  w.style.visibility = "visible";
+  a.style.color = "white";
+  b.style.color = "black";
+}
+  
+function google() {
+  	window.location.assign("https://accounts.google.com/signin/v2/identifier?service=accountsettings&continue=https%3A%2F%2Fmyaccount.google.com%2F%3Futm_source%3Dsign_in_no_continue&csig=AF-SEnbZHbi77CbAiuHE%3A1585466693&flowName=GlifWebSignIn&flowEntry=AddSession", "_blank");
+}
+
+// for side menu in index.html line no 37
+function sideMenu(side) {
+  var menu = document.getElementById('side-menu');
+  if(side==0) {
+    menu.style = 'transform: translateX(0vh); position:fixed;';
+  }
+  else {
+    menu.style = 'transform: translateX(-100%);';
+  }
+  side++;
+}
